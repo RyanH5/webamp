bb33a554acebaa068a582f2bda7a8e382d69f763
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-22 19:11:05 +0000

    Deploy captbaritone/webamp to github.com/captbaritone/webamp.git:gh-pages

---
+++ precache-manifest.261256c27f56fd2e25ebbdb652a65ba3.js
@@ -0,0 +1,81 @@
+self.__precacheManifest = [
+  {
+    "url": "/images/preview-42304ea56f49c60e3d2c6c97d1bdddeb.png"
+  },
+  {
+    "url": "/images/manifest/icon_192x192.ec9b715176586bf58ec5abe0354efd85.png"
+  },
+  {
+    "url": "/images/manifest/icon_512x512.5228b937afc3368434b594aa65be2c08.png"
+  },
+  {
+    "url": "/images/manifest/icon_513x513.1f27cb4bba614c00bf595696efa4435d.svg"
+  },
+  {
+    "url": "/milkdrop-preset-converter-323f123a84e26477ce80.js"
+  },
+  {
+    "url": "/milkdrop-preset-converter-323f123a84e26477ce80.js.map"
+  },
+  {
+    "url": "/jszip-323f123a84e26477ce80.js"
+  },
+  {
+    "url": "/jszip-323f123a84e26477ce80.js.map"
+  },
+  {
+    "url": "/manifest.60fc98cc18ea0b3ab073cda74610efa1.json"
+  },
+  {
+    "revision": "83e61ba777fb69fe35b2847f810bf414",
+    "url": "/index.html"
+  },
+  {
+    "url": "/webamp-323f123a84e26477ce80.js"
+  },
+  {
+    "url": "/webamp-323f123a84e26477ce80.js.map"
+  },
+  {
+    "url": "/images/share/butterchurn-bg-d09543a6c6d8150469b722454c583c07.png"
+  },
+  {
+    "url": "/images/manifest/icon-192x192-fd139d3437e0cad44d05f85b226a24d7.png"
+  },
+  {
+    "url": "/butterchurn-initial-dependencies-323f123a84e26477ce80.js.map"
+  },
+  {
+    "url": "/images/favicon-0cc27932e7d5cbda2849b27e6e6dc3c8.ico"
+  },
+  {
+    "url": "/images/manifest/icon-48x48-67470808290acc080f2d154fd1df70d1.png"
+  },
+  {
+    "url": "/images/manifest/icon-72x72-9d9aaf3f4dad41359616e32e082dc71c.png"
+  },
+  {
+    "url": "/images/manifest/icon-96x96-44fece775401fd6184d71022778b4edd.png"
+  },
+  {
+    "url": "/css/page-6266f8359de1c8eb51d3c465e7c33e94.css"
+  },
+  {
+    "url": "/images/manifest/icon-144x144-06bb87f22f8080f239f096e856435678.png"
+  },
+  {
+    "url": "/butterchurn-non-minimal-presets-323f123a84e26477ce80.js.map"
+  },
+  {
+    "url": "/butterchurn-non-minimal-presets-323f123a84e26477ce80.js"
+  },
+  {
+    "url": "/butterchurn-initial-dependencies-323f123a84e26477ce80.js"
+  },
+  {
+    "url": "/1-323f123a84e26477ce80.js.map"
+  },
+  {
+    "url": "/1-323f123a84e26477ce80.js"
+  }
+];
